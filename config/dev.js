4d5f0351913f980db0caacb719bc71cb33ba814d
Camilo <camilochafloque@gmail.com>
2023-04-24 23:55:33 -0500

    Testing with Jest finished

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://chafo:chafo123@blogapp.qazqjlh.mongodb.net/?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisURI:
+    "redis://default:admin123@redis-17297.c239.us-east-1-2.ec2.cloud.redislabs.com:17297",
+};
---
+++ services/cache.js
@@ -0,0 +1,46 @@
+const redis = require("redis");
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const exec = mongoose.Query.prototype.exec;
+const client = redis.createClient({ url: keys.redisURI });
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.connect();
+
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "default");
+  return this; // * So you can chain this function with other query params
+};
+
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) return exec.apply(this, arguments);
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
+
+  // * See if we have a value of key in redis
+  const cacheValue = await client.hGet(this.hashKey, key);
+
+  // * If we do, return that
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // * Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  client.hSet(this.hashKey, key, JSON.stringify(result), "EX", 10);
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
+};
