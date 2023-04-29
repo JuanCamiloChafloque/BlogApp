d0da21f267236c485a624926438c35c27ad5b1f7
Camilo <camilochafloque@gmail.com>
2023-04-29 13:39:51 -0500

    Added upload image route with S3 bucket config

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,28 @@
+const aws = require("aws-sdk");
+const uuid = require("uuid/v4");
+const keys = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+  },
+  region: "us-east-2",
+});
+
+module.exports = (app) => {
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = req.user.id + "/" + uuid() + ".jpeg";
+
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "blog-app-bucket",
+        ContentType: "jpeg",
+        Key: key,
+      },
+      (err, url) => res.send({ key, url })
+    );
+  });
+};
