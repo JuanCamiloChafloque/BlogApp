a2c886d473b3ad283680e619cb7e420a7cd05a18
Camilo <camilochafloque@gmail.com>
2023-04-29 14:14:28 -0500

    Increased individual tests timeouts

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,17 +12,20 @@ afterEach(async () => {
 });
 
 test("Header logo has correct text", async () => {
+  test.setTimeout(120000);
   const text = await page.getContentsOf("a.brand-logo");
   expect(text).toEqual("Blogster");
 });
 
 test("Clicking login start oAuth flow", async () => {
+  test.setTimeout(120000);
   await page.click(".right a");
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
 test("When signed-in, shows logout button", async () => {
+  test.setTimeout(120000);
   await page.login();
   const text = await page.getContentsOf("a[href='/auth/logout']");
   expect(text).toEqual("Logout");
